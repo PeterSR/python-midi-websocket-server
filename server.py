27d0e0f3e9043a3cacb0ce23f4aa13121ecc6711
Peter Severin Rasmussen <git@petersr.com>
2020-04-02 11:01:20 +0200

    Experimented with simple websockets (python server, js client)

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+async def echo(websocket, path):
+    async for message in websocket:
+        print(message)
+        await websocket.send(message)
+
+start_server = websockets.serve(echo, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
